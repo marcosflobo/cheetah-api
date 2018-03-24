86f801ba4ebd07d60bf4fa2cf010851b796cb2b5
Marcos Lobo <marcos.lobo@nexthink.com>
2018-03-24 18:33:17 +0100

    Added flask framework and ping call

--- cheetahapi/main.py
+++ cheetahapi/main.py
@@ -1,6 +1,16 @@
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+
+@app.route("/ping")
+def ping():
+    return "cheeta-api version 1.0 up and running!"
+
+
 def main():
     return 1
 
 
 if __name__ == "__main__":
-    main()
+    app.run(host='0.0.0.0', port=80)

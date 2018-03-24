a7be7996f415c167be654b1d28e747423c7f8243
Marcos Lobo <marcos.lobo@nexthink.com>
2018-03-24 18:12:09 +0100

    Initial tests for authentication module

---
+++ cheetahapi/core/authenticate.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+
+
+class Authenticate(object):
+    """
+    Number of days that a token is valid. In the future, this value will come from config file
+    """
+    token_days_valid = 5
+
+    def authenticate(self, user, password):
+        """
+        Authenticates a user using username and password
+        :param user: Username
+        :param password: Password for the username
+        :return: Token object with crated date and user_id
+        """
+        user = self.get_user_from_db(user, password)
+        if user is None:
+            raise Exception("User '{0}' not found using password".format(user))
+        token = self.get_token_user_id(user["id"])
+        if token is None or not self.is_valid_token(token["token"]):
+            token = self.create_new_token(user["id"])
+        return token
+
+    def get_user_from_db(self, user, password):
+        # TODO
+        pass
+
+    def create_new_token(self, param):
+        # TODO
+        pass
+
+    def get_token_user_id(self, param):
+        # TODO
+        """
+
+        :param param:
+        :return:
+        """
+        pass
+
+    def is_valid_token(self, token):
+        """
+        Checks if the token string is valid, which means the creation date does not exceed the amount of days allowed
+        by configuration file
+        :param token: Token string
+        :return: True if the token string is valid, False in other case
+        """
+        token_obj = self.get_token_from_db(token)
+        return self.token_date_not_expired(token_obj["date"])
+
+    def token_date_not_expired(self, token_date_creation):
+        """
+        Checks the token date creation does not exceed the number of days allowed
+        :param token_date_creation: Date as string
+        :return: True if the date does NOT exceed the number of days from today, False in other case
+        """
+        datetime_object = datetime.strptime(token_date_creation, "%Y-%m-%d")
+        return (self.get_today_date() - datetime_object).days <= self.get_token_days_valid()
+
+    def get_today_date(self):
+        return datetime.today()
+
+    def get_token_from_db(self, token):
+        # TODO
+        pass
+
+    def get_token_days_valid(self):
+        return self.token_days_valid
+
+    def set_token_days_valid(self, num_days):
+        self.token_days_valid = num_days
---
+++ tests/authenticate/test_authenticate.py
@@ -0,0 +1,140 @@
+from datetime import datetime
+from unittest import TestCase
+import mock
+
+from cheetahapi.core.authenticate import Authenticate
+
+
+class TestAuthenticate(TestCase):
+    """Tests for authenticate module."""
+
+    exp_token = {"token": "foo-token", "date": "2018-03-24", "user_id": "1"}
+    user_id = "1"
+    user = "moe"
+    pw = "pass"
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_today_date",
+                return_value=datetime.strptime("2018-03-24", "%Y-%m-%d"))
+    def test_token_has_valid_date(self, mock_get_today_date):
+        """Test to check when the token has a valid date and it does not exceed the number of days when it's valid"""
+        token_creation_date_string = "2018-03-23"
+        auth = Authenticate()
+        auth.set_token_days_valid(1)
+
+        ret = auth.token_date_not_expired(token_creation_date_string)
+        mock_get_today_date.assert_called_once()
+        self.assertTrue(ret)
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_today_date",
+                return_value=datetime.strptime("2018-03-24", "%Y-%m-%d"))
+    def test_token_has_not_valid_date(self, mock_get_today_date):
+        """Test to check when the token has not a valid date and exceeds the number of days when it's valid"""
+        token_creation_date_string = "2018-03-22"
+        auth = Authenticate()
+        auth.set_token_days_valid(1)
+
+        ret = auth.token_date_not_expired(token_creation_date_string)
+        mock_get_today_date.assert_called_once()
+        self.assertFalse(ret)
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_today_date",
+                return_value=datetime.strptime("2018-03-24", "%Y-%m-%d"))
+    def test_is_valid_token(self, mock_get_today_date):
+        """Test a token is valid"""
+        auth = Authenticate()
+        auth.set_token_days_valid(1)
+        exp_get_token_from_db = {"date": "2018-03-23"}
+        with mock.patch("cheetahapi.core.authenticate.Authenticate.get_token_from_db",
+                        return_value=exp_get_token_from_db):
+            is_valid = auth.is_valid_token(self.exp_token)
+        mock_get_today_date.assert_called_once()
+        self.assertTrue(is_valid)
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_today_date",
+                return_value=datetime.strptime("2018-03-24", "%Y-%m-%d"))
+    def test_is_invalid_token(self, mock_get_today_date):
+        """Test a token is not valid"""
+        auth = Authenticate()
+        auth.set_token_days_valid(1)
+        exp_get_token_from_db = {"date": "2018-03-22"}
+        with mock.patch("cheetahapi.core.authenticate.Authenticate.get_token_from_db",
+                        return_value=exp_get_token_from_db):
+            is_valid = auth.is_valid_token(self.exp_token)
+            mock_get_today_date.assert_called_once()
+        self.assertFalse(is_valid)
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.create_new_token")
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.is_valid_token",
+                return_value=True)
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_token_user_id",
+                return_value={"token": "foo-token", "date": "2018-03-24", "user_id": "1"})
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_user_from_db",
+                return_value={"user": "john", "id": "1"})
+    def test_authenticate_ok(self, mock_get_user_from_db, mock_get_token_user_id,
+                             mock_is_valid_token, mock_create_new_token):
+        """Test to check authentication process is working"""
+        auth = Authenticate()
+
+        token = auth.authenticate(self.user, self.pw)
+        self.assertFalse(mock_create_new_token.called)
+        mock_get_user_from_db.assert_called_once_with(self.user, self.pw)
+        mock_get_token_user_id.assert_called_once_with(self.user_id)
+        mock_is_valid_token.assert_called_once_with(self.exp_token["token"])
+        self.assertEqual(self.exp_token["token"], token["token"])
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.create_new_token",
+                return_value={"token": "foo-token", "date": "2018-03-24", "user_id": "1"})
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.is_valid_token",
+                return_value=True)
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_token_user_id", return_value=None)
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_user_from_db",
+                return_value={"user": "john", "id": "1"})
+    def test_authenticate_ok_create_new_token_from_none_token(self, mock_get_user_from_db, mock_get_token_user_id,
+                                                              mock_is_valid_token, mock_create_new_token):
+        """Test to check authentication process is working creating a new token because there was not previous token"""
+        auth = Authenticate()
+
+        token = auth.authenticate(self.user, self.pw)
+        mock_get_user_from_db.assert_called_once_with(self.user, self.pw)
+        mock_get_token_user_id.assert_called_once_with(self.user_id)
+        mock_create_new_token.assert_called_once_with(self.user_id)
+        self.assertFalse(mock_is_valid_token.called)
+        self.assertEqual(self.exp_token["token"], token["token"])
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.create_new_token",
+                return_value={"token": "foo-token", "date": "2018-03-24", "user_id": "1"})
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.is_valid_token",
+                return_value=False)
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_token_user_id",
+                return_value={"token": "foo-token", "date": "2018-03-24", "user_id": "1"})
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_user_from_db",
+                return_value={"user": "john", "id": "1"})
+    def test_authenticate_ok_create_new_token_from_invalid_token(self, mock_get_user_from_db, mock_get_token_user_id,
+                                                                 mock_is_valid_token, mock_create_new_token):
+        """Test to check authentication process is working creating a new token because previous token expired"""
+        auth = Authenticate()
+
+        token = auth.authenticate(self.user, self.pw)
+        mock_get_user_from_db.assert_called_once_with(self.user, self.pw)
+        mock_get_token_user_id.assert_called_once_with(self.user_id)
+        mock_create_new_token.assert_called_once_with(self.user_id)
+        mock_is_valid_token.assert_called_once_with(self.exp_token["token"])
+        self.assertEqual(self.exp_token["token"], token["token"])
+
+    @mock.patch("cheetahapi.core.authenticate.Authenticate.get_user_from_db",
+                return_value=None)
+    def test_authenticate_error_wrong_user_or_passwd(self, mock_get_user_from_db):
+        auth = Authenticate()
+        exp_token = token = 0
+        try:
+            token = auth.authenticate(self.user, self.pw)
+            # To be sure that the exception is raised
+            self.assertTrue(1 == 0)
+        except Exception:
+            pass
+        self.assertEqual(exp_token, token)
+        mock_get_user_from_db.assert_called_once_with(self.user, self.pw)
+
+
+if __name__ == '__main__':
+    unittest.main()
